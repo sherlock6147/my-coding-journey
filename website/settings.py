434c52df259fbd609a57ee8573a4993e1a9a239c
md manzer alam <tooshort9541@gmail.com>
2021-06-14 12:04:03 +0530

    added password pages and email

--- website/settings.py
+++ website/settings.py
@@ -150,3 +150,10 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'me.mycodingjourney@gmail.com'
+from .secrets import password
+EMAIL_HOST_PASSWORD = password
